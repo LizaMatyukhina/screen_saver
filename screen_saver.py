96c6d9cbeb0f50ce7bc89a1d8c939fca27ffb890
LizaMatyukhina <44421806+LizaMatyukhina@users.noreply.github.com>
2019-11-12 13:26:24 +0700

    last commit maybe

--- screen_saver.py
+++ screen_saver.py
@@ -60,7 +60,7 @@ class Line:
             if self.points[point].y > SCREEN_SIZE[1] or self.points[point].y < 0:
                 self.speeds[point] = Vector(self.speeds[point].x, -self.speeds[point].y)
 
-    def draw_points(self, points=None, style="points", width=4, color=(255, 255, 255)):
+    def draw_points(self, gameDisplay, points=None, style="points", width=4, color=(255, 255, 255)):
         if style == "line":
             for point_number in range(-1, len(points) - 1):
                 pygame.draw.line(gameDisplay, color, points[point_number].int_pair(),
@@ -124,84 +124,92 @@ class Joint(Line):
         return result
 
 
-def display_help():
-    gameDisplay.fill((50, 50, 50))
-    font1 = pygame.font.SysFont("arial", 30)
-    font2 = pygame.font.SysFont("serif", 30)
-    data = []
-    data.append(["F1", "Помощь"])
-    data.append(["R", "Перезапуск"])
-    data.append(["P", "Воспроизвести / Пауза"])
-    data.append(["Num+", "Добавить точку"])
-    data.append(["Num-", "Удалить точку"])
-    data.append(["", ""])
-    data.append([str(steps), "текущих точек"])
+class Main:
 
-    pygame.draw.lines(gameDisplay, (255, 50, 50, 255), True, [
-        (0, 0), (800, 0), (800, 600), (0, 600)], 5)
-    for item, text in enumerate(data):
-        gameDisplay.blit(font1.render(
-            text[0], True, (128, 128, 255)), (100, 100 + 30 * item))
-        gameDisplay.blit(font2.render(
-            text[1], True, (128, 128, 255)), (200, 100 + 30 * item))
+    @staticmethod
+    def start():
+        pygame.init()
+        gameDisplay = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Screen Saver")
+
+        steps = 20
+        working = True
+        points = []
+        speeds = []
+        show_help = False
+        pause = False
+        color_param = 0
+        color = pygame.Color(0)
+
+        joint = Joint(steps)
+
+        while working:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    working = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        working = False
+                    if event.key == pygame.K_r:
+                        joint = Joint(steps)
+                    if event.key == pygame.K_p:
+                        pause = not pause
+                    if event.key == pygame.K_KP_PLUS:
+                        steps += 1
+                    if event.key == pygame.K_F1:
+                        show_help = not show_help
+                    if event.key == pygame.K_KP_MINUS:
+                        steps -= 1 if steps > 1 else 0
+
+                    if event.key == pygame.K_DELETE:
+                        if len(joint.points) > 3:
+                            joint.delete(-1)
+                    if event.key == pygame.K_UP:
+                        joint.speed_up(2)
+                    if event.key == pygame.K_DOWN:
+                        joint.speed_up(0.5)
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    joint.add_point(Vector(*event.pos), Vector(random() * 2, random() * 2))
+            gameDisplay.fill((0, 0, 0))
+            color_param = (color_param + 1) % 360
+            color.hsla = (color_param, 100, 50, 100)
+            joint.draw_points(gameDisplay)
+            joint.draw_points(gameDisplay, joint.get_joint(), "line", 4, color)
+            if not pause:
+                joint.set_points()
+            if show_help:
+                Main.display_help(gameDisplay, steps)
+
+            pygame.display.flip()
+
+        pygame.display.quit()
+        pygame.quit()
+        exit(0)
+
+    @staticmethod
+    def display_help(gameDisplay, steps):
+        gameDisplay.fill((50, 50, 50))
+        font1 = pygame.font.SysFont("arial", 30)
+        font2 = pygame.font.SysFont("serif", 30)
+        data = []
+        data.append(["F1", "Помощь"])
+        data.append(["R", "Перезапуск"])
+        data.append(["P", "Воспроизвести / Пауза"])
+        data.append(["Num+", "Добавить точку"])
+        data.append(["Num-", "Удалить точку"])
+        data.append(["", ""])
+        data.append([str(steps), "текущих точек"])
+
+        pygame.draw.lines(gameDisplay, (255, 50, 50, 255), True, [
+            (0, 0), (800, 0), (800, 600), (0, 600)], 5)
+        for item, text in enumerate(data):
+            gameDisplay.blit(font1.render(
+                text[0], True, (128, 128, 255)), (100, 100 + 30 * item))
+            gameDisplay.blit(font2.render(
+                text[1], True, (128, 128, 255)), (200, 100 + 30 * item))
 
 
 if __name__ == "__main__":
-    pygame.init()
-    gameDisplay = pygame.display.set_mode(SCREEN_SIZE)
-    pygame.display.set_caption("Screen Saver")
-
-    steps = 20
-    working = True
-    points = []
-    speeds = []
-    show_help = False
-    pause = False
-    color_param = 0
-    color = pygame.Color(0)
-
-    joint = Joint(steps)
-
-    while working:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                working = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    working = False
-                if event.key == pygame.K_r:
-                    joint = Joint(steps)
-                if event.key == pygame.K_p:
-                    pause = not pause
-                if event.key == pygame.K_KP_PLUS:
-                    steps += 1
-                if event.key == pygame.K_F1:
-                    show_help = not show_help
-                if event.key == pygame.K_KP_MINUS:
-                    steps -= 1 if steps > 1 else 0
-
-                if event.key == pygame.K_DELETE:
-                    if len(joint.points) > 3:
-                        joint.delete(-1)
-                if event.key == pygame.K_UP:
-                    joint.speed_up(2)
-                if event.key == pygame.K_DOWN:
-                    joint.speed_up(0.5)
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                joint.add_point(Vector(*event.pos), Vector(random() * 2, random() * 2))
-        gameDisplay.fill((0, 0, 0))
-        color_param = (color_param + 1) % 360
-        color.hsla = (color_param, 100, 50, 100)
-        joint.draw_points()
-        joint.draw_points(joint.get_joint(), "line", 4, color)
-        if not pause:
-            joint.set_points()
-        if show_help:
-            display_help()
-
-        pygame.display.flip()
-
-    pygame.display.quit()
-    pygame.quit()
-    exit(0)
+    screen_saver = Main()
+    screen_saver.start()
